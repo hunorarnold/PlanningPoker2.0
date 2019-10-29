7cb6ec5dbee6a5cfeeed1d641feee6ea42f85691
Hunor Vargas <45651881+hunorvargas@users.noreply.github.com>
2019-10-29 21:06:34 +0200

    Vargas added 4 object

---
+++ app/src/main/java/com/example/planningpoker20/Session.java
@@ -0,0 +1,54 @@
+package com.example.planningpoker20;
+
+import java.util.ArrayList;
+
+public class Session {
+
+   private String id;
+   private String sessionName;
+   private ArrayList< Question > questions;
+   private ArrayList< User > users;
+
+    @Override
+    public String toString() {
+        return "Session{" +
+                "id=" + id +
+                ", sessionName='" + sessionName + '\'' +
+                ", questions=" + questions +
+                ", users=" + users +
+                '}';
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSessionName() {
+        return sessionName;
+    }
+
+    public void setSessionName(String sessionName) {
+        this.sessionName = sessionName;
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(ArrayList<Question> questions) {
+        this.questions = questions;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+}
---
+++ app/src/main/java/com/example/planningpoker20/User.java
@@ -0,0 +1,46 @@
+package com.example.planningpoker20;
+
+public class User {
+
+    private int userId;
+    private String userName;
+    private String userAnswer;
+
+    public User(int userId, String userName) {
+        this.userId = userId;
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", userAnswer='" + userAnswer + '\'' +
+                '}';
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAnswer() {
+        return userAnswer;
+    }
+
+    public void setUserAnswer(String userAnswer) {
+        this.userAnswer = userAnswer;
+    }
+}
